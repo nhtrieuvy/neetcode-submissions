5f552b5c848e440dec58e62da7bb6f58735c8e81
Nguyen Vy <142155561+nhtrieuvy@users.noreply.github.com>
2026-07-01 17:10:40 +0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n*2);
+        
+        for(int i = 0; i < nums.size(); i++){
+            res[i] = nums[i];
+            res[i + n] = nums[i];
+        }
+        return res;
+    }
+};
